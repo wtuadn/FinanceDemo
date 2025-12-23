f8b3c4cadc8ba4a4dcea5c6448d515427700e56b
maoyu <maoyu@cloudview.mobi>
2025-12-23 19:01:03 +0800

    更新一波ETF

--- app/src/test/java/com/example/myapplication/SinaFinance.kt
+++ app/src/test/java/com/example/myapplication/SinaFinance.kt
@@ -23,20 +23,16 @@ class SinaFinance {
 
     @Test
     fun main() {
-        // val symbol = SymbolData(code = "sh588000", desc = "科创50ETF")
         // val symbol = SymbolData(code = "sz159915", desc = "易方达创业板ETF")
         // val symbol = SymbolData(code = "sh518880", desc = "黄金ETF")
         // val symbol = SymbolData(code = "sz159201", desc = "华夏国证自由现金流")
-        // val symbol = SymbolData(code = "sh515450", desc = "南方红利低波50")
         // val symbol = SymbolData(code = "sh515100", desc = "红利低波100ETF")
         // val symbol = SymbolData(code = "sh512890", desc = "红利低波ETF")
-        // val symbol = SymbolData(code = "sh510880", desc = "上证红利ETF")
-        val symbol = SymbolData(code = "sz159941", desc = "纳指ETF广发")
+        // val symbol = SymbolData(code = "sz159941", desc = "纳指ETF广发")
         // val symbol = SymbolData(code = "sh513130", desc = "恒生科技ETF")
         // val symbol = SymbolData(code = "sz159892", desc = "恒生医药ETF")
-        // val symbol = SymbolData(code = "sz159545", desc = "恒生红利低波ETF")
+        val symbol = SymbolData(code = "sz159545", desc = "恒生红利低波ETF")
         // val symbol = SymbolData(code = "sh515050", desc = "5G通信ETF")
-        // val symbol = SymbolData(code = "sh512660", desc = "军工ETF")
 
         calculateBestMAArgs(symbol)
         // calculateSpecificMAArg(
@@ -60,7 +56,7 @@ class SinaFinance {
             var kLineData = getKLineData(symbol, scale, d)
             // kLineData = kLineData.filterNot { it.date.split("-").first().toInt() < 2021 }
             println("--- scale=$scale d=$d 有效数据时间段为：${kLineData.firstOrNull()?.date} - ${kLineData.lastOrNull()?.date} ---\n")
-            listOf(MA_TYPE.SMA,/* MA_TYPE.EMA*/).forEach { maType ->
+            listOf(MA_TYPE.SMA, MA_TYPE.EMA).forEach { maType ->
                 listOf(1, 5, 10, 15, 20, 25, 30).forEach { shortMA ->
                     listOf(5, 10, 15, 20, 25, 30, 40, 60, 120, 180).forEach { longMA ->
                         if (shortMA >= longMA) return@forEach
@@ -94,7 +90,7 @@ class SinaFinance {
                 }
             }
         }
-        list.removeAll { it.first.totalCrossData.totalCount.toDouble() / it.first.yearCrossDataMap.size < 1 } //过滤掉操作次数太少的
+        // list.removeAll { it.first.totalCrossData.totalCount.toDouble() / it.first.yearCrossDataMap.size < 1 } //过滤掉操作次数太少的
         println("\n\n年收益优先")
         repeat(10) { println("--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---") }
         list.sortedByDescending { it.first.yearCrossDataMap.values.minOf { it.totalPercentage } }
@@ -158,6 +154,33 @@ class SinaFinance {
         println("\n$args \n${result} \n${result.totalCrossData.crossDataList.joinToString("\n")}")
     }
 
+    // --- 参数：sz159915 易方达创业板ETF scale=240 d=1 shortMA=1 longMA=25 maType:SMA upCrossMADiffRate=0.04 downCrossMADiffRate=0.0 ---
+    // 2021 总次数: 3 胜率: 100.00% 涨幅: 11.52% 最大涨幅: 8.08% 最大回撤: 0.00%
+    // 2022 总次数: 2 胜率: 50.00% 涨幅: 7.40% 最大涨幅: 11.38% 最大回撤: -3.99%
+    // 2023 总次数: 1 胜率: 100.00% 涨幅: 1.46% 最大涨幅: 1.46% 最大回撤: 0.00%
+    // 2024 总次数: 3 胜率: 66.67% 涨幅: 34.37% 最大涨幅: 35.41% 最大回撤: -3.05%
+    // 2025 总次数: 4 胜率: 25.00% 涨幅: 32.52% 最大涨幅: 39.12% 最大回撤: -3.44%
+    // total: 平均年涨幅17.45% 总次数: 13 胜率: 61.54% 涨幅: 87.26% 最大涨幅: 39.12% 最大回撤: -3.99%
+
+    // --- 参数：sh515050 5G通信ETF scale=240 d=1 shortMA=1 longMA=20 maType:SMA upCrossMADiffRate=0.01 downCrossMADiffRate=0.0 ---
+    // 2020 总次数: 9 胜率: 33.33% 涨幅: 8.78% 最大涨幅: 13.55% 最大回撤: -4.81%
+    // 2021 总次数: 9 胜率: 44.44% 涨幅: 9.39% 最大涨幅: 8.11% 最大回撤: -3.45%
+    // 2022 总次数: 6 胜率: 33.33% 涨幅: 5.33% 最大涨幅: 7.38% 最大回撤: -2.31%
+    // 2023 总次数: 11 胜率: 27.27% 涨幅: 6.82% 最大涨幅: 13.38% 最大回撤: -4.74%
+    // 2024 总次数: 8 胜率: 37.50% 涨幅: 31.48% 最大涨幅: 21.18% 最大回撤: -3.32%
+    // 2025 总次数: 7 胜率: 42.86% 涨幅: 82.48% 最大涨幅: 89.82% 最大回撤: -3.13%
+    // total: 平均年涨幅24.05% 总次数: 50 胜率: 36.00% 涨幅: 144.28% 最大涨幅: 89.82% 最大回撤: -4.81%
+
+    // --- 参数：sh512890 红利低波ETF scale=240 d=1 shortMA=1 longMA=15 maType:SMA upCrossMADiffRate=0.0 downCrossMADiffRate=-0.01 ---
+    // 2019 总次数: 5 胜率: 60.00% 涨幅: 7.07% 最大涨幅: 6.71% 最大回撤: -1.48%
+    // 2020 总次数: 9 胜率: 55.56% 涨幅: 15.50% 最大涨幅: 13.65% 最大回撤: -3.71%
+    // 2021 总次数: 7 胜率: 42.86% 涨幅: 19.29% 最大涨幅: 15.55% 最大回撤: -3.04%
+    // 2022 总次数: 11 胜率: 45.45% 涨幅: 10.32% 最大涨幅: 6.38% 最大回撤: -3.36%
+    // 2023 总次数: 10 胜率: 30.00% 涨幅: 3.79% 最大涨幅: 5.96% 最大回撤: -2.24%
+    // 2024 总次数: 9 胜率: 44.44% 涨幅: 7.46% 最大涨幅: 5.71% 最大回撤: -2.73%
+    // 2025 总次数: 6 胜率: 50.00% 涨幅: 10.32% 最大涨幅: 8.41% 最大回撤: -2.30%
+    // total: 平均年涨幅10.54% 总次数: 57 胜率: 45.61% 涨幅: 73.75% 最大涨幅: 15.55% 最大回撤: -3.71%
+
     // --- 参数：sh515100 红利低波100ETF scale=240 d=1 shortMA=10 longMA=15 maType:SMA upCrossMADiffRate=0.0 downCrossMADiffRate=0.0 ---
     // 2021 总次数: 5 胜率: 60.00% 涨幅: 13.50% 最大涨幅: 8.96% 最大回撤: -0.99%
     // 2022 总次数: 10 胜率: 50.00% 涨幅: 2.40% 最大涨幅: 6.77% 最大回撤: -3.39%
@@ -166,16 +189,20 @@ class SinaFinance {
     // 2025 总次数: 9 胜率: 77.78% 涨幅: 6.30% 最大涨幅: 2.39% 最大回撤: -0.64%
     // total: 平均年涨幅9.05% 总次数: 46 胜率: 50.00% 涨幅: 45.26% 最大涨幅: 14.02% 最大回撤: -3.74%
 
-    // --- 参数：sh513130 恒生科技ETF scale=240 d=1 shortMA=1 longMA=5 maType:SMA upCrossMADiffRate=0.04 downCrossMADiffRate=0.0 ---
-    // 2022 总次数: 8 胜率: 87.50% 涨幅: 26.37% 最大涨幅: 10.30% 最大回撤: -3.44%
-    // 2023 总次数: 9 胜率: 33.33% 涨幅: 2.97% 最大涨幅: 7.63% 最大回撤: -2.39%
-    // 2024 总次数: 7 胜率: 42.86% 涨幅: 31.23% 最大涨幅: 24.44% 最大回撤: -3.18%
-    // 2025 总次数: 9 胜率: 33.33% 涨幅: 6.64% 最大涨幅: 12.61% 最大回撤: -3.52%
-    // total: 平均年涨幅16.80% 总次数: 33 胜率: 48.48% 涨幅: 67.20% 最大涨幅: 24.44% 最大回撤: -3.52%
+    // --- 参数：sz159201 华夏国证自由现金流 scale=240 d=1 shortMA=1 longMA=5 maType:SMA upCrossMADiffRate=0.0 downCrossMADiffRate=-0.03 ---
+    // 2025 总次数: 2 胜率: 50.00% 涨幅: 25.63% 最大涨幅: 26.04% 最大回撤: -0.40%
+    // total: 平均年涨幅25.63% 总次数: 2 胜率: 50.00% 涨幅: 25.63% 最大涨幅: 26.04% 最大回撤: -0.40%
 
     // --- 参数：sz159545 恒生红利低波ETF scale=240 d=1 shortMA=5 longMA=10 maType:SMA upCrossMADiffRate=0.01 downCrossMADiffRate=0.0 ---
     // 2025 总次数: 5 胜率: 80.00% 涨幅: 23.94% 最大涨幅: 17.15% 最大回撤: -1.11%
     // total: 平均年涨幅23.94% 总次数: 5 胜率: 80.00% 涨幅: 23.94% 最大涨幅: 17.15% 最大回撤: -1.11%
+
+    // --- 参数：sh513130 恒生科技ETF scale=240 d=1 shortMA=5 longMA=30 maType:SMA upCrossMADiffRate=0.01 downCrossMADiffRate=0.0 ---
+    // 2022 总次数: 1 胜率: 100.00% 涨幅: 6.78% 最大涨幅: 6.78% 最大回撤: 0.00%
+    // 2023 总次数: 4 胜率: 50.00% 涨幅: 23.94% 最大涨幅: 29.20% 最大回撤: -7.44%
+    // 2024 总次数: 4 胜率: 100.00% 涨幅: 25.86% 最大涨幅: 20.12% 最大回撤: 0.00%
+    // 2025 总次数: 6 胜率: 50.00% 涨幅: 17.53% 最大涨幅: 19.08% 最大回撤: -3.43%
+    // total: 平均年涨幅18.53% 总次数: 15 胜率: 66.67% 涨幅: 74.11% 最大涨幅: 29.20% 最大回撤: -7.44%
 
     // --- 参数：sz159892 恒生医药ETF scale=240 d=1 shortMA=10 longMA=20 maType:SMA upCrossMADiffRate=0.01 downCrossMADiffRate=-0.01 ---
     // 2022 总次数: 2 胜率: 50.00% 涨幅: 18.92% 最大涨幅: 25.46% 最大回撤: -6.55%
@@ -245,20 +272,16 @@ class SinaFinance {
     @Test
     fun fetchJsonFile() {
         val symbol = listOf(
-            "sh588000",
             "sz159915",
             "sh518880",
             "sz159201",
-            "sh515450",
             "sh515100",
             "sh512890",
-            "sh510880",
             "sz159941",
             "sh513130",
             "sz159892",
             "sz159545",
             "sh515050",
-            "sh512660",
         )
         val scale = 240
         val d = listOf(1, 5)
